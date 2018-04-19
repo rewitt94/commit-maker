b2d9621144513f4c3ea905fa781d0a9e643449aa
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-04-19 14:37:06 +0100

    Completely manual commit

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 var exec = require('child_process').exec;
 
-/**
- * Execute simple shell command (async wrapper).
- * @param {String} cmd
- * @return {Object} { stdout: String, stderr: String }
- */
-
-async function sh(cmd) {
+function sh(cmd) {
   return new Promise(function (resolve, reject) {
     exec(cmd, (err, stdout, stderr) => {
       if (err) {
@@ -18,23 +12,23 @@ async function sh(cmd) {
   });
 }
 
-async function runSh(cmd) {
-  // let { stdout } = await sh(cmd);
-  //
-  // }
+function handleSh(cmd) {
+  return new Promise((resolve,reject) => {
+    sh(cmd)
+      .then((stdout) => {
+        // for (let line of stdout.split('\n')) {
+          console.log(`stdout: ${stdout}`);
+        // }
+        resolve()
+      })
+      .catch((err) => {
+        console.log(err)
+        reject()
+      })
+    })
 }
 
-// runSh('echo "this is a new file" > new.txt')
-// runSh('git add .')
-// runSh('git commit -m "Automatic commit"')
-// runSh('git push')
-
-sh('git add .')
-  .then((stdout) => {
-    console.log(stdout)
-  // for (let line of stdout.split('\n')) {
-  //   console.log(`stdout: ${line}`);
-  // }
-    sh('git commit -m "Automatic commit"')
-  .catch((err) => console.log(err))
-})
+handleSh('echo "this is a new text file" > file.txt')
+ .then(() => handleSh('git add .'))
+ .then(() => handleSh('git commit -m "Completely manual commit"'))
+ .then(() => handleSh('git push'))
